bef0663039c9fdcb1aeef8cb5307a1312900ce35
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2023-05-29 01:43:13 +0900

    Calculate quickxorhash and show base64

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{BufReader, Read},
+    path::PathBuf,
+};
+
+use base64::{prelude::BASE64_STANDARD, Engine};
+use clap::Parser;
+use fs_err::File;
+use quickxorhash::QuickXorHash;
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let f = File::open(args.input)?;
+    let mut reader = BufReader::new(f);
+
+    let mut buffer = [0u8; 0x100000];
+    let mut hasher = QuickXorHash::new();
+    loop {
+        match reader.read(&mut buffer)? {
+            0 => break,
+            n => hasher.update(&buffer[..n]),
+        }
+    }
+
+    let res = hasher.finalize();
+    let res = BASE64_STANDARD.encode(res);
+    println!("{res}");
+
+    Ok(())
+}
+
+#[derive(Parser)]
+struct Args {
+    input: PathBuf,
 }
